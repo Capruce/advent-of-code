84c47a2b7ed64e4b0a455196f43e0e766278081f
Luis Morera De La Cruz <l.moreradelacruz@iwoca.co.uk>
2021-12-10 21:42:47 +0000

    Simplify day 10.

--- 2021/day_10/main.py
+++ 2021/day_10/main.py
@@ -1,5 +1,6 @@
 import fileinput
 from functools import reduce
+from statistics import median
 
 OPEN  = ["(", "[", "{", "<"]
 CLOSE = [")", "]", "}", ">"]
@@ -13,21 +14,15 @@ if __name__ == "__main__":
     for line in lines:
         stack = []
         for symbol in line:
-            try:
-                index = OPEN.index(symbol)
-            except ValueError:
-                if stack.pop(-1) != (index := CLOSE.index(symbol)):
-                    syntax_error_score += SCORE[index]
-                    break
-            else:
-                stack.append(index)
+            if symbol in OPEN:
+                stack.append(OPEN.index(symbol))
+            elif stack.pop(-1) != (index := CLOSE.index(symbol)):
+                syntax_error_score += SCORE[index]
+                break
         else:
             autocomplete_scores.append(
                 reduce(lambda t, i: (t * 5) + i + 1, reversed(stack), 0)
             )
 
     print("Part 1:", syntax_error_score)
-
-    autocomplete_scores = sorted(autocomplete_scores)
-    middle = autocomplete_scores[len(autocomplete_scores) // 2]
-    print("Part 2:", middle)
+    print("Part 2:", median(autocomplete_scores))
